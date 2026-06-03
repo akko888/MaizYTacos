8b80ed5412c392c4805a26bf566b124a8210bac4
Akko888 <axuval123@gmail.com>
2026-06-03 00:43:35 -0600

    Firs basic menu complementation I added a pretty minimal working menu, I hope you'all like the designs! uwu

--- Scripts/Scene/menus.gd
+++ Scripts/Scene/menus.gd
@@ -15,3 +15,15 @@ func go_to(screen: String) -> void:
 	for s in screens.values():
 		s.hide()
 	screens[screen].show()
+
+
+# Buttons functions
+
+func _on_start_button_pressed() -> void:
+	go_to("mode")
+
+func _on_vs_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/scene.tscn")
+
+func _on_back_button_pressed() -> void:
+	go_to("title")
